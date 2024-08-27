8fd7eb16b1df575c0c9dee7ff93f4903970eb98d
stalin670 <amitalvin01@gmail.com>
2024-08-28 03:30:38 +0530

    Created API and Schema Models

--- client/src/Pages/Register.jsx
+++ client/src/Pages/Register.jsx
@@ -1,43 +1,64 @@
-import React, { useState } from 'react'
-// import { useNavigate } from 'react-router-dom'
-import '../style/register.css'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "../style/register.css";
 
-export const Register = (event) => {
+export const Register = () => {
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  // const navigate = useNavigate()
+
+  const handleSignup = async (event) => {
     event.preventDefault();
 
-    const [name, setName] = useState()
-    const [email, setEmail] = useState()
-    const [password, setPassword] = useState()
-    // const navigate = useNavigate()
+    try {
+      const response = await fetch("http://localhost:4000/api/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name,
+          email,
+          password,
+        }),
+      });
 
-    async function handleSignup() {
-        const response = await fetch('http://localhost:4000/api/register', {
-            method : 'POST',
-            headers : {
-                'Content-Type' : 'application/json',
-            },
-            body : JSON.stringify({
-                name,
-                email, 
-                password,
-            }),
-        })
+      const data = await response.json();
 
-        const data = await response.json();
-
-        console.log(data);
+      console.log(data);
+    } catch (error) {
+      console.log(error);
     }
+  };
 
   return (
     <div className="Form-Container">
         <h2>Sign Up</h2>
-        <input type='text' value={name} onChange={(e) => setName(e.target.value)} placeholder='Name' />
-        <input type='email' value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Email' />
-        <input type='password' value={password} onChange={(e) => setPassword(e.target.value)} placeholder='Password' />
-        <button onClick={handleSignup}>Signup</button>
+      <form onSubmit={handleSignup}>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Name"
+        />
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+        />
+        <button type="submit">Signup</button>
+        </form>
         <p>
-            Already have an account ? <button>Login</button>
+          Already have an account ? <button>Login</button>
         </p>
     </div>
-  )
-}
+  );
+};
