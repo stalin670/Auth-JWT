d7fa6f811c996361d3e3c379083981d1fe1fea67
stalin670 <amitalvin01@gmail.com>
2024-08-28 04:28:05 +0530

    Added JSONWEBTOKEN(JWT) Functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
 import { Register } from './Pages/Register';
+import { Login } from './Pages/Login'
+import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom'
 
 function App() {
   return (
-    <Register />
+    <Router>
+      <Routes>
+      <Route path='/' element={<Navigate to='/Login' />} />
+        <Route path='/Login' element={<Login />} />
+        <Route path='/Register' element={<Register />} />
+      </Routes>
+    </Router>
   );
 }
 
--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -7,12 +7,37 @@ export const Login = () => {
     const [password, setPassword] = useState()
     // const navigate = useNavigate()
 
+    const handleLogin = async (event) => {
+        event.preventDefault();
+    
+        try {
+          const response = await fetch("http://localhost:4000/api/login", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              email,
+              password,
+            }),
+          });
+    
+          const data = await response.json();
+    
+          console.log(data);
+        } catch (error) {
+          console.log(error);
+        }
+      };
+
   return (
     <div className="Form-Container">
         <h2>Login</h2>
+        <form onSubmit={handleLogin}>
         <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
         <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
-        <button onClick={handleLogin} >Login</button>
+        <button type='submit' >Login</button>
+        </form>
         <p>
             Don't have an account ? <button >Signup</button>
         </p>
