ea6f3e38b882adfd9ac842dd7cb21a5a1aa42f64
stalin670 <amitalvin01@gmail.com>
2024-09-10 04:32:41 +0530

    Update User Model

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -5,7 +5,7 @@ const User = new mongoose.Schema(
         userName : { type : String, required : true },
         email : { type : String, required : true, unique : true },
         password : { type : String, required : true },
-        quote : { type : String },
+        quote : { type : String, default: "" },
     },
     {
         collection : 'user-data'
